8a5c710acfef07850dcdfe254ed956a1990a3512
abhilash <abhilashv8877@gmail.comgit config --global user.email abhilashv8877@gmail.com>
2021-10-20 09:51:58 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,12 @@
+// function displayValue(num){
+//     let res=document.querySelector("#result")
+//     res.value+=num
+// }
+var displayValue=(num)=>result.value+=num
+var clearBox=()=>result.value=""
+// function evaluateExpression(){
+//     let expr=result.value
+//     let out=eval(expr)
+//     result.value=out
+// }
+var evaluateExpression=()=>result.value=eval(result.value)
